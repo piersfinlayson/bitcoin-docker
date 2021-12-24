227c574a88b9725d4a50b5b577dccc9086eb5f68
Piers Finlayson <piers@piersandkatie.com>
2021-12-24 06:28:11 +0000

    don't login until built image

--- build.armv7l.sh
+++ build.armv7l.sh
@@ -8,7 +8,6 @@ then
 else
 	echo Building version $VERSION
 fi
-docker login -u piersfinlayson
 docker build -f Dockerfile.arm --build-arg VERSION=$VERSION --target piersfinlayson/bitcoin-armv7l:$VERSION && docker push piersfinlayson/bitcoin-armv7l:$VERSION
 docker tag piersfinlayson/bitcoin-armv7l:$VERSION piersfinlayson/bitcoin-armv7l:latest
 docker login -u piersfinlayson
