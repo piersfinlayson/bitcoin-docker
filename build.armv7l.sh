43fbdb5861fa2ff6459a87e845878e056b58465b
Piers Finlayson <piers@piersandkatie.com>
2021-12-24 06:30:07 +0000

    add path£

--- build.armv7l.sh
+++ build.armv7l.sh
@@ -8,7 +8,7 @@ then
 else
 	echo Building version $VERSION
 fi
-docker build -f Dockerfile.arm --build-arg VERSION=$VERSION --target bitcoin-armv7l -t piersfinlayson/bitcoin-armv7l:$VERSION
+docker build --build-arg VERSION=$VERSION --target bitcoin-armv7l -t piersfinlayson/bitcoin-armv7l:$VERSION -f Dockerfile.arm .
 docker tag piersfinlayson/bitcoin-armv7l:$VERSION piersfinlayson/bitcoin-armv7l:latest
 docker login -u piersfinlayson
 docker push piersfinlayson/bitcoin-armv7l:$VERSION
