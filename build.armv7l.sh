1576b2f29818814fd647352ee829f43005875d6c
Piers Finlayson <piers@piersandkatie.com>
2021-12-24 06:29:38 +0000

    correct build

--- build.armv7l.sh
+++ build.armv7l.sh
@@ -8,7 +8,7 @@ then
 else
 	echo Building version $VERSION
 fi
-docker build -f Dockerfile.arm --build-arg VERSION=$VERSION --target piersfinlayson/bitcoin-armv7l:$VERSION && docker push piersfinlayson/bitcoin-armv7l:$VERSION
+docker build -f Dockerfile.arm --build-arg VERSION=$VERSION --target bitcoin-armv7l -t piersfinlayson/bitcoin-armv7l:$VERSION
 docker tag piersfinlayson/bitcoin-armv7l:$VERSION piersfinlayson/bitcoin-armv7l:latest
 docker login -u piersfinlayson
 docker push piersfinlayson/bitcoin-armv7l:$VERSION
