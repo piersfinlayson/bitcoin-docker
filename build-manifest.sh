c3a047d34a342d1ec8747af94161a67536c7595b
Piers Finlayson <piers@piersandkatie.com>
2021-12-26 08:44:01 +0000

    fix bugs

--- build-manifest.sh
+++ build-manifest.sh
@@ -17,23 +17,19 @@ else
 	echo Building container version $CONT_VERSION
 fi
 
-VERSION=`cat VERSION`
-echo "Create piersfinlayson/bitcoin manifests"
-echo "       version: ${VERSION}"
-
 docker login
 
 # Create VERSION manifest
-docker manifest create -a piersfinlayson/bitcoin:${VERSION} piersfinlayson/bitcoin-amd64:${VERSION} piersfinlayson/bitcoin-armv7l:${VERSION}
-docker manifest annotate --arch amd64 --os linux piersfinlayson/bitcoin:${VERSION} piersfinlayson/bitcoin-amd64:${VERSION}
-docker manifest annotate --arch arm --os linux --variant armv7l piersfinlayson/bitcoin:${VERSION} piersfinlayson/bitcoin-armv7l:${VERSION}
-docker manifest inspect piersfinlayson/bitcoin:${VERSION}
-docker manifest push --purge piersfinlayson/bitcoin:${VERSION}
+docker manifest create -a piersfinlayson/bitcoin:${CONT_VERSION} piersfinlayson/bitcoin-amd64:${CONT_VERSION} piersfinlayson/bitcoin-armv7l:${CONT_VERSION}
+docker manifest annotate --arch amd64 --os linux piersfinlayson/bitcoin:${CONT_VERSION} piersfinlayson/bitcoin-amd64:${CONT_VERSION}
+docker manifest annotate --arch arm --os linux --variant armv7l piersfinlayson/bitcoin:${CONT_VERSION} piersfinlayson/bitcoin-armv7l:${CONT_VERSION}
+docker manifest inspect piersfinlayson/bitcoin:${CONT_VERSION}
+docker manifest push --purge piersfinlayson/bitcoin:${CONT_VERSION}
 
 # Create latest manifest
-docker manifest create -a piersfinlayson/bitcoin:latest piersfinlayson/bitcoin-amd64:${VERSION} piersfinlayson/bitcoin-armv7l:${VERSION}
-docker manifest annotate --arch amd64 --os linux piersfinlayson/bitcoin:latest piersfinlayson/bitcoin-amd64:${VERSION}
-docker manifest annotate --arch arm --os linux --variant armv7l piersfinlayson/bitcoin:latest piersfinlayson/bitcoin-armv7l:${VERSION}
+docker manifest create -a piersfinlayson/bitcoin:latest piersfinlayson/bitcoin-amd64:${CONT_VERSION} piersfinlayson/bitcoin-armv7l:${CONT_VERSION}
+docker manifest annotate --arch amd64 --os linux piersfinlayson/bitcoin:latest piersfinlayson/bitcoin-amd64:${CONT_VERSION}
+docker manifest annotate --arch arm --os linux --variant armv7l piersfinlayson/bitcoin:latest piersfinlayson/bitcoin-armv7l:${CONT_VERSION}
 docker manifest inspect piersfinlayson/bitcoin:latest
 docker manifest push --purge piersfinlayson/bitcoin:latest
 
