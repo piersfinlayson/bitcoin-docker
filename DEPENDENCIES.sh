b4ff8f9848865a72446326600ebb9948f11a2e59
Piers Finlayson <piers@piersandkatie.com>
2023-10-08 07:35:13 +0100

    uprev openssl

--- DEPENDENCIES.sh
+++ DEPENDENCIES.sh
@@ -5,4 +5,4 @@
 LIBEVENT_VERSION=2.1.12
 LIBZMQ_VERSION=4.3.4
 BOOST_VERSION=1.81.0
-BC_OPENSSL_VERSION="1.1.1v" # OPENSSL_VERSION already defined in piersfinlayson/build
+BC_OPENSSL_VERSION="1.1.1w" # OPENSSL_VERSION already defined in piersfinlayson/build
